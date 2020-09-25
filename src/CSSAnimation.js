e8561cdbb76a238acda5c21fec7868b987781de4
Jacob <jacobbuck@users.noreply.github.com>
2020-09-25 22:59:49 +1200

    Update import of react to use named exports (rather than default export)

--- src/CSSAnimation.js
+++ src/CSSAnimation.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import * as React from 'react';
 import { Transition } from 'react-transition-group';
 import normalizeTime from './utils/normalizeTime';
 import defaultProps from './utils/defaultProps';
